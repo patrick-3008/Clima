44b1f48b7793fcfe270618ca520bd8e127af6894
Patrick <patricknaashat@yahoo.com>
2023-09-21 18:55:50 +0300

    add weather from json model and some renaming

---
+++ Clima/Model/WeatherJson.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+struct WeatherJson: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let description: String
+}
--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -3,7 +3,7 @@ import Foundation
 struct WeatherManager {
     let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=935d391d38090c00a2e92f2a0bac251c&units=metric"
     
-    func fetchWeather(cityName: String){
+    func fetchWeather(cityName: String) {
         let urlString = "\(weatherUrl)&q=\(cityName)"
         performRequest(urlString: urlString)
     }
@@ -34,11 +34,16 @@ struct WeatherManager {
     func parseJSON(weatherData: Data){
         let decoder = JSONDecoder()
         do {
-           let decodedData = try decoder.decode(WeatherModel.self, from: weatherData)
-            print(decodedData.main.temp)
+           let decodedData = try decoder.decode(WeatherJson.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            weather.conditionName
         } catch {
             print(error)
         }
     }
-    
+        
 }
